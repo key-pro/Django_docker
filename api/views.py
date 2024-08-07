6bad5981b8e7a3203c49175c600c4328b128fd17
key-pro <taka65000@yahoo.co.jp>
2024-08-07 23:09:39 +0900

    株価　API　簡易実装　ベーシック認証実装

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view, authentication_classes, permission_classes  # 必要なデコレーターをインポート
+from rest_framework.authentication import BasicAuthentication  # ベーシック認証をインポート
+from rest_framework.permissions import IsAuthenticated  # 認証を必要とするパーミッションをインポート
 import yfinance as yf  # yfinanceをインポート
 
 @api_view()
@@ -7,6 +10,8 @@ def hello_world(request):
     return Response({"message": "Hello, world!"})
 
 @api_view(['GET'])
+@authentication_classes([BasicAuthentication])  # ベーシック認証を追加
+@permission_classes([IsAuthenticated])  # 認証を必要とするパーミッションを追加
 def stock_price(request):
     # クエリパラメーターから値を取得
     symbol = request.query_params.get('symbol', 'AAPL')  # デフォルト値を設定
